6a79233cd9b8c2e32252a6050cda9afee2053db6
Fredrik Mile <fredrik.mile@gmail.com>
2023-12-04 15:51:25 +0100

    clean solution for day04

--- day04.py
+++ day04.py
@@ -1,28 +1,25 @@
+from collections import defaultdict
 with open("input.txt") as f:
     input = f.read().splitlines()
-
 
 s = 0
 for line in input:
     line = line.split(":")[1]
     wns, mns = line.split("|")
 
-    same = set(wns.split()).intersection(set(mns.split()))
-    if len(same) > 0:
-        s += 2 ** (len(same) - 1)
+    intersection = set(wns.split()) & set(mns.split())
+    s += 2 ** (len(intersection) - 1) if len(intersection) > 0 else 0
 
 print(s)
 
-
 s = 0
-rows_won = {i: 1 for i in range(1, len(input) + 1)}
-
+d = defaultdict(int)
 for i, line in enumerate(input, start=1):
+    d[i] += 1
     line = line.split(":")[1]
     wns, mns = line.split("|")
+    intersection = set(wns.split()) & set(mns.split())
+    for j in range(i + 1, i + len(intersection) + 1):
+        d[j] += d[i]
 
-    same = set(wns.split()).intersection(set(mns.split()))
-    for j in range(i + 1, i + len(same) + 1):
-        rows_won[j] = rows_won[j] + rows_won[i]
-
-print(sum(rows_won.values()))
+print(sum(d.values()))
