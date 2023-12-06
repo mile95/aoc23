d0f8fbf9a0e5c0b86c6a633aa438a711e169b6c5
Fredrik Mile <fredrik.mile@gmail.com>
2023-12-06 06:31:04 +0100

    clean up part two day06

--- day06.py
+++ day06.py
@@ -19,27 +19,13 @@ for t, d in zip(times, distances):
 print(math.prod(counts))
 
 
-time = ""
-distance = ""
+time = int("".join([x for x in input[0].split(":")[1].split()]))
+distance = int("".join([x for x in input[1].split(":")[1].split()]))
 
-for t in times:
-    time += str(t)
+s = 0
+for x in range(0, time + 1):
+    c = x * (time - x)
+    if c > distance:
+        s += 1
 
-for d in distances:
-    distance += str(d)
-
-time = [int(time)]
-distance = [int(distance)]
-
-
-counts = []
-for t, d in zip(time, distance):
-    s = 0
-    for x in range(0, t + 1):
-        c = x * (t - x)
-        if c > d:
-            s += 1
-
-    counts.append(s)
-
-print(math.prod(counts))
+print(s)
